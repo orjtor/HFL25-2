d3c60d9db5c3bb206170ca69ea8e690ed0041b9a
orjtor <orjan.torang@gmail.com>
2025-10-06 00:35:10 +0200

    WIP 2 with test

--- v02/test/v02_test.dart
+++ v02/test/v02_test.dart
@@ -2,7 +2,24 @@ import 'package:v02/v02.dart';
 import 'package:test/test.dart';
 
 void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
+  group('Hero', () {
+    test('toMap and fromMap', () {
+      final hero = Hero(name: 'Batman', power: 'Money', strength: 85);
+      final map = hero.toMap();
+      expect(map, {'name': 'Batman', 'power': 'Money', 'strength': 85});
+
+      final newHero = Hero.fromMap(map);
+      expect(newHero.name, 'Batman');
+      expect(newHero.power, 'Money');
+      expect(newHero.strength, 85);
+    });
+
+    test('fromMap with missing fields', () {
+      final map = {'name': 'Superman'};
+      final hero = Hero.fromMap(map);
+      expect(hero.name, 'Superman');
+      expect(hero.power, '');
+      expect(hero.strength, 0);
+    });
   });
 }
